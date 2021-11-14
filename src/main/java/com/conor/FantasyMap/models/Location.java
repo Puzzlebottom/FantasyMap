5597be6b893668eafff524f99b5e71ff9cf75600
Conor Meldrum <conor.j.meldrum@gmail.com>
2021-11-14 11:38:14 -0500

    streamlines Location.setCoordsFromOriginByVector()

--- src/main/java/com/conor/FantasyMap/models/Location.java
+++ src/main/java/com/conor/FantasyMap/models/Location.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.Arrays;
+import java.util.List;
 
 @Entity
 @Table(uniqueConstraints = {@UniqueConstraint(name = "UniqueCoords", columnNames = {"xCoord", "yCoord"})})
@@ -20,33 +21,16 @@ public class Location {
     @GeneratedValue
     private Long id;
 
+    private static List<Integer> cardinalDegrees = List.of(0, 45, 90, 135, 180, 225, 270, 315, 360);
+    private static List<String> cardinalValues = List.of("N", "NE", "E", "SE", "S", "SW", "W", "NW", "N");
+
     public void setCoordsFromOriginByVector(Location origin, String direction, Integer distance) {
-        Integer x = origin.getXCoord();
-        Integer y = origin.getYCoord();
-        switch (direction) {
-            case "N" -> y += distance;
-            case "NE" -> {
-                x += distance;
-                y += distance;
-            }
-            case "E" -> x += distance;
-            case "SE" -> {
-                x += distance;
-                y -= distance;
-            }
-            case "S" -> y -= distance;
-            case "SW" -> {
-                x -= distance;
-                y -= distance;
-            }
-            case "W" -> x -= distance;
-            case "NW" -> {
-                x -= distance;
-                y += distance;
-            }
-        }
-        this.setXCoord(x);
-        this.setYCoord(y);
+        int index = cardinalValues.indexOf(direction);
+        double angle = Math.toRadians(cardinalDegrees.get(index));
+        int destinationX = (int) Math.round(origin.getXCoord() + distance * Math.cos(angle));
+        int destinationY = (int) Math.round(origin.getYCoord() + distance * Math.sin(angle));
+        this.setXCoord(destinationX);
+        this.setYCoord(destinationY);
     }
 
     public String calculateBearingTo(Location destination) {
@@ -55,17 +39,15 @@ public class Location {
         int x2 = destination.getXCoord();
         int y2 = destination.getYCoord();
 
-        int angle = (int) Math.toDegrees(Math.atan2(x2 - x1, y2 - y1));
+        int angle = (int) Math.toDegrees(Math.atan2(y2 - y1, x2 - x1));
         if (angle < 0) {
             angle += 360;
         }
 
-        Integer[] cardinalBreakpoints = new Integer[] {0, 45, 90, 135, 180, 225, 270, 315, 360};
-        String[] cardinalValues = new String[] {"N", "NE", "E", "SE", "S", "SW", "W", "NW", "N"};
-        int cardinalOffset = (360/Arrays.stream(cardinalValues).distinct().toArray().length)/2;
+        int cardinalOffset = (360/cardinalValues.stream().distinct().toArray().length)/2;
         int finalAngle = angle;
-        int index = Arrays.stream(cardinalBreakpoints).filter(i -> finalAngle > (i - cardinalOffset)).toArray().length - 1;
-        return cardinalValues[index];
+        int index = cardinalDegrees.stream().filter(i -> finalAngle > (i - cardinalOffset)).toArray().length - 1;
+        return cardinalValues.get(index);
     }
 
     public Integer calculateDistanceTo(Location destination) {
