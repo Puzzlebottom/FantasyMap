8899f5b3170460733592b5bd9a6745a14659d44a
Conor Meldrum <conor.j.meldrum@gmail.com>
2022-07-18 16:44:58 -0400

    fixes travelLog sorting bug

--- src/test/java/com/conor/FantasyMap/presenters/TravelLogTest.java
+++ src/test/java/com/conor/FantasyMap/presenters/TravelLogTest.java
@@ -2,6 +2,7 @@ package com.conor.FantasyMap.presenters;
 
 import com.conor.FantasyMap.models.Location;
 import com.conor.FantasyMap.models.LogEntry;
+import com.conor.FantasyMap.models.LogEntryType;
 import com.conor.FantasyMap.models.Point;
 import org.junit.jupiter.api.Test;
 
@@ -9,13 +10,26 @@ import java.util.List;
 
 import static com.conor.FantasyMap.models.LogEntryFactory.createLogEntryByCourse;
 import static com.conor.FantasyMap.models.LogEntryFactory.createLogEntryByDestination;
+import static com.conor.FantasyMap.models.LogEntryType.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class TravelLogTest {
     @Test
     void getFormattedTravelLogShouldReturnListOfFormattedMovements() {
-        List<LogEntry> logEntries = List.of(createLogEntryByCourse("N", 24),
-                createLogEntryByCourse("E", 24));
+        LogEntry mockEntry1 = mock(LogEntry.class);
+        when(mockEntry1.getId()).thenReturn(1L);
+        when(mockEntry1.getDirectionName()).thenReturn("north");
+        when(mockEntry1.getDeltaHours()).thenReturn(24);
+        when(mockEntry1.getType()).thenReturn(COURSE);
+        LogEntry mockEntry2 = mock(LogEntry.class);
+        when(mockEntry2.getId()).thenReturn(2L);
+        when(mockEntry2.getDirectionName()).thenReturn("east");
+        when(mockEntry2.getDeltaHours()).thenReturn(24);
+        when(mockEntry2.getType()).thenReturn(COURSE);
+        List<LogEntry> logEntries = List.of(mockEntry1, mockEntry2);
+
         TravelLog travelLog = new TravelLog(logEntries);
 
         assertThat(travelLog.getFormattedTravelLog()).containsExactly("Party travelled north for 24 hours", "Party travelled east for 24 hours");
